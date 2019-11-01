f2dddf96d81271105dce08bf5670072bf04e8d33
Campbell <campbellim@38f9d3c6e634.chop.edu>
2019-11-01 15:25:03 -0400

    Implemented re-ordering

--- Lesson-Generator/app.R
+++ Lesson-Generator/app.R
@@ -8,24 +8,24 @@ library(shinyjqui)
 #Source functions for URL encoding and decoding
 source("URL-Encode.R")
 
-#Load tables from disk
-module_table <- fread("ModuleTable.csv")
-# module_table <- fread("/Users/campbellim/Arcus/Lessons/ModuleTable.csv")
-curricula_table <- fread("CurriculaTable.csv")
-# curricula_table <- fread("/Users/campbellim/Arcus/Curricula/CurriculaTable.csv")
-
-master_table <- rbind(curricula_table[,list(type="Curriculum",key=curriculum_key,title,default_modules)],module_table[,list(type="Module",key=lesson_key,title,default_modules=NA)])
-master_table[,display:=paste0(type,": ",title)]
-setkey(master_table,display)
-
-values <- reactiveValues(selected_items=vector(),selected_type=vector(),current_modules=vector(),modules_per_curriculum=vector())
-
 ui <- fluidPage(
     uiOutput("page_output")
 )
 
 server <- function(input, output, session) {
 
+  #Load tables from disk
+  module_table <- fread("ModuleTable.csv")
+  #module_table <- fread("/Users/campbellim/Arcus/Lessons/ModuleTable.csv")
+  curricula_table <- fread("CurriculaTable.csv")
+  #curricula_table <- fread("/Users/campbellim/Arcus/Curricula/CurriculaTable.csv")
+
+  master_table <- rbind(curricula_table[,list(type="Curriculum",key=curriculum_key,title,default_modules)],module_table[,list(type="Module",key=lesson_key,title,default_modules=NA)])
+  master_table[,display:=paste0(type,": ",title)]
+  setkey(master_table,display)
+  vals<-reactiveValues()
+  vals$Data<- master_table[,list(type,title,key)]
+  values <- reactiveValues(selected_items=vector(),selected_type=vector(),current_modules=vector(),modules_per_curriculum=vector())
         output$page_output <- renderUI({
 
         fluidRow(column("",width=2),column(
@@ -40,15 +40,38 @@ server <- function(input, output, session) {
           column(selectizeInput(inputId = "main_input", label="Curricula", choices = master_table[type=="Curriculum",display], multiple = TRUE),width=6),
           column(selectizeInput(inputId = "module_input", label="Modules", choices = master_table[type=="Module",display], multiple = TRUE),width=6)
           ),
-        renderTable(master_table[values$selected_items,list(type,title,key)]),
+        #renderTable(master_table[values$selected_items,list(type,title,key)]),
+        dataTableOutput("Main_table"),
         actionButton("clear", "Clear"),
         actionButton("gen", "Generate URL"),
         tags$br(),
         tags$br(),
         tags$h3("Custom URL:"),
         textInput("url_holder", label = "", value = "",width="70%")
-        , width=8),column("",width=2))
+        , width=8),column("",width=2),
+        tags$script("$(document).on('click', '#Main_table button', function () {
+                    Shiny.onInputChange('lastClickId',this.id);
+                    Shiny.onInputChange('lastClick', Math.random())
+  });"))
     })
+
+        output$Main_table<-renderDataTable({
+          if(length(values$selected_items)==0){DT <- as.data.table(list(type=rep("",2),title=rep("",2),key=rep("",2)))}
+          else {DT=master_table[values$selected_items,list(type,title,key)]}
+
+
+          DT[["Actions"]]<-
+            paste0('
+             <div class="btn-group" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-secondary up"id=up_',1:nrow(master_table[values$selected_items,]),'>⬆︎</button>
+                <button type="button" class="btn btn-secondary down"id=down_',1:nrow(master_table[values$selected_items,]),'>⬇︎</button>
+                <button type="button" class="btn btn-secondary delete" id=delete_',1:nrow(master_table[values$selected_items,]),'>✕︎</button>
+             </div>
+
+             ')
+          datatable(DT,
+                    escape=F,rownames=F,options = list(dom='t',ordering=F,pageLength=-1))}
+        )
 
     observeEvent(input$main_input,{
         values$selected_type <<- master_table[input$main_input,type]
@@ -63,7 +86,7 @@ server <- function(input, output, session) {
             values$current_modules <- c(input$main_input,master_table[type == "Module" & key %in% current_default_modules,display])
             values$selected_items <<- append(values$selected_items,values$current_modules)
         }
-        #Otherwise, just added the selection directly
+        #Otherwise, just add the selection directly
         else
         {values$selected_items <<- append(values$selected_items,input$main_input)}
 
@@ -75,6 +98,42 @@ server <- function(input, output, session) {
 
         updateSelectizeInput(session, "main_input", selected = "")
     })
+
+    observeEvent(input$lastClick,
+                 {
+                   if (input$lastClickId%like%"delete")
+                   {
+                     row_to_del=as.numeric(gsub("delete_","",input$lastClickId))
+                     #vals$Data=vals$Data[-row_to_del]
+                     values$selected_items=values$selected_items[-row_to_del]
+                   }
+                   else if (input$lastClickId%like%"up")
+                   {
+                     row_to_up=as.numeric(gsub("up_","",input$lastClickId))
+                     raiseRow <- function(vec,pos){
+                       temp <- vec ;
+                       if(pos == 1) {return(vec)}
+                       else if (pos > length(vec)) {return(vec)}
+                       else { temp[pos-1] <- pos; temp[pos] <- vec[pos-1]; return(temp)}
+                     }
+                     new_order <- raiseRow(1:nrow(master_table[values$selected_items,]),row_to_up)
+                     #vals$Data=vals$Data[new_order]
+                     values$selected_items=values$selected_items[new_order]
+                   }
+                   else if (input$lastClickId%like%"down")
+                   {
+                     row_to_down=as.numeric(gsub("down_","",input$lastClickId))
+                     lowerRow <- function(vec,pos){
+                       temp <- vec ;
+                       if(pos >= length(vec)) {return(vec)}
+                       else {temp[pos+1] <- pos; temp[pos] <- vec[pos+1]; return(temp)}
+                     }
+                     new_order <- lowerRow(1:nrow(master_table[values$selected_items,]),row_to_down)
+                     #vals$Data=vals$Data[new_order]
+                     values$selected_items=values$selected_items[new_order]
+                   }
+                 }
+    )
 
     observeEvent(input$module_input,{
       values$selected_type <<- master_table[input$module_input,type]
